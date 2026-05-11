7b2caa26080362a4084f54b6fe75e844e07eb9a5
islamsorifulhero <islamsoriful.hero@gmail.com>
2026-05-11 21:12:30 +0600

    Package.json updated with tsx installed

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,11 @@
-import { createServer } from "http";
+import { createServer, IncomingMessage, ServerResponse } from "http";
 
-const server = createServer
+const server = createServer(
+    (req: IncomingMessage, res: ServerResponse) => {
+        console.log(req);
+    }
+)
+
+server.listen(5000, () => {
+    console.log('Server is running on the port 5000');
+})
