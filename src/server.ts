198967503dcd1de8e0ab5aba9cb77bc8e9feb217
MD Soriful Islam <islamsoriful.hero@gmail.com>
2026-05-15 23:20:51 +0600

    server side updated

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,12 @@
-// import { createServer, IncomingMessage, ServerResponse } from "http";
+import { createServer, IncomingMessage, ServerResponse } from "http";
 
-// const server = createServer(
-//     (req: IncomingMessage, res: ServerResponse) => {
-//         const url = req.url;
-//         const method = req.method;
-//     }
-// )
+const server = createServer(
+    (req: IncomingMessage, res: ServerResponse) => {
+        const url = req.url;
+        const method = req.method;
+    }
+)
 
-// server.listen(5000, () => {
-//     console.log('Server is running on the port 5000');
-// })
-
-console.log("adfslafsdlkdsa")
+server.listen(5000, () => {
+    console.log('Server is running on the port 5000');
+})
