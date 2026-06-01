87814f530f3eecd573e03ff7af8166804052a9ad
MD Soriful Islam <islamsoriful.hero@gmail.com>
2026-06-01 22:48:48 +0600

    Product type updated and unic id, name, description, price added

--- src/types/product.type.ts
+++ src/types/product.type.ts
@@ -1,3 +1,10 @@
+// export interface IProduct {
+//   id: number;
+//   name: string;
+//   description: string;
+//   price: number;
+// }
+
 export interface IProduct {
   id: number;
   name: string;
