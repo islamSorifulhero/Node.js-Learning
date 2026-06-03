2916713e2ebbc9625cdaae76e95ee132c5621356
MD Soriful Islam <islamsoriful.hero@gmail.com>
2026-06-03 08:21:07 +0600

    Send Response updated with res, statusCode, success, message etc

--- src/utility/sendResponse.ts
+++ src/utility/sendResponse.ts
@@ -16,3 +16,22 @@ export const sendResponse = (
   res.writeHead(statusCode, { "content-type": "application/json" });
   res.end(JSON.stringify(response));
 };
+
+
+// import type { ServerResponse } from "http";
+
+// export const sendResponse = (
+//   res: ServerResponse,
+//   statusCode: number,
+//   success: boolean,
+//   message: string,
+//   data?: any,
+// ) => {
+//   const response = {
+//     success,
+//     message,
+//     data,
+//   };
+//   res.writeHead(statusCode, { "content-type": "application/json" });
+//   res.end(JSON.stringify(response));
+// };
