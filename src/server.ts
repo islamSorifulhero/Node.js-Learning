24764656f8c175d3dbb9f46117278ee0c2284a3e
MD Soriful Islam <islamsoriful.hero@gmail.com>
2026-06-04 21:52:40 +0600

    Server side updated and createServer IncomingMessage added

--- src/server.ts
+++ src/server.ts
@@ -10,3 +10,16 @@ const server = createServer(
 server.listen(5000, () => {
     console.log('Server is running on the port 5000');
 })
+
+// import { createServer, IncomingMessage, ServerResponse } from "http";
+
+// const server = createServer(
+//     (req: IncomingMessage, res: ServerResponse) => {
+//         const url = req.url;
+//         const method = req.method;
+//     }
+// )
+
+// server.listen(5000, () => {
+//     console.log('Server is running on the port 5000');
+// })
